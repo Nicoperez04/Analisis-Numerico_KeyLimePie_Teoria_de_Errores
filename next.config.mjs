1fc228732812570016521af744568b41e272eb32
nicolasperez <nicoapgelp@gmail.com>
2025-09-08 20:53:31 -0300

    fix(pages): usar solo basePath y nojekyll

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
-const repo = 'Analisis-Numerico_KeyLimePie_Teoria_de_Errores'
+const repo = 'KeyLimePie_Errores'
 
 const nextConfig = {
   output: 'export',
   basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
   trailingSlash: true,
   images: { unoptimized: true },
   eslint: { ignoreDuringBuilds: true },
